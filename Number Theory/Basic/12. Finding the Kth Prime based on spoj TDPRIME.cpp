97abdf61d607a81dd0a2ca6697f2a375e907841f
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2020-04-01 12:02:10 +0600

    findingKthPrime

---
+++ Number Theory/Basic/12. Finding the Kth Prime based on spoj TDPRIME.cpp
@@ -0,0 +1,73 @@
+
+// </> : towhid1zaman
+
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;typedef vector<ll> vll;
+#define fill(a) memset(a, 0, sizeof (a))
+#define all(v) (v).begin(),(v).end()
+#define sp(k) cout<<setprecision(k)<<fixed;
+#define rep(i,a) for(int i=0; i<a;i++)
+#define rep1(i,a,b) for(int i=(a);i<=(b);++i)
+#define irep(i,b,a) for(int i=(b);i>=(a);--i)
+#define minv(v) *min_element(v.begin(),v.end())
+#define maxv(v) *max_element(v.begin(),v.end())
+#define each(it,s) for(auto it = s.begin(); it != s.end(); ++it)
+#define unq(v) sort(all(v)),(v).erase(unique((v).begin(),(v).end()),(v).end())
+#define getpos(v,x) (lower_bound(v.begin(),v.end(),x)-v.begin())
+#define _ios ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define sqr(a) ((a)*(a))
+#define sz(a) int(a.size())
+#define ff first
+#define ss second
+#define pb push_back
+
+const int INF = 1e9 + 10;
+const double pi = acos(-1.0);
+const int mod = 1000000007; // (int)1e9+7
+const int maxn = 90000000;
+
+vi prime;
+bool is_prime[90000001];
+
+void seive(){
+    int N = 90000000;
+    is_prime[0] = is_prime[1]=true; // false means prime number and true means composite
+    for(int i = 2; i*i<=N; i++){
+        if(!is_prime[i]){
+            for(int j = i*i; j<=N; j+=i){
+                is_prime[j] = true;
+            }
+        }
+    }
+
+    for(int i = 2; i<=N; i++){
+        if(!is_prime[i]){
+            prime.push_back(i);
+        }
+    }
+}
+
+int main(){
+        _ios;
+
+
+        int T; cin >> T;
+
+        seive();
+        while(T--){
+            int  q; cin >> q;
+            cout<<prime[q-1]<<endl;
+        }
+
+return 0;
+}
+
+
+
+
+
+
