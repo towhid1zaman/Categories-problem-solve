043bc548a9ed4295f0fb89b242922c96bf591401
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2020-04-02 13:02:15 +0600

    Update 3. Binary Exponentiation (for long x,n (x^n) ) module m.cpp

--- Number Theory/Basic/3. Binary Exponentiation (for long x,n (x^n) ) module m.cpp
+++ Number Theory/Basic/3. Binary Exponentiation (for long x,n (x^n) ) module m.cpp
@@ -1,1 +1,28 @@
 
+ //   CF: Tman
+
+#include "bits/stdc++.h"
+using namespace std;
+#define ll long long
+#define mod 100000007
+ll bxpo(ll x,ll n){
+    ll res = 1;
+    while(n>0){
+        if(n%2==1){
+            res = (res*x)%mod;
+        }
+        x = (x*x)%mod;
+        n = n/2;
+    }
+    return res;
+}
+
+int main()
+{
+    ll n,x;
+    while(cin>>x>>n){
+        cout<<"Exponentiation using BExp "<<bxpo(x,n)<<endl;
+    }
+
+return 0;
+}
