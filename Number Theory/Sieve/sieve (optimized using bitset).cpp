0c82fc422d5d326be89c491d63648edaa7e4015a
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2020-04-03 14:09:02 +0600

    O(n*log(log(n)))

---
+++ Number Theory/Sieve/sieve (optimized using bitset).cpp
@@ -0,0 +1,64 @@
+
+// </> : towhid1zaman
+
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;typedef vector<ll> vll;
+#define fill(a) memset(a, 0, sizeof (a))
+#define all(v) (v).begin(),(v).end()
+#define sp(k) cout<<setprecision(k)<<fixed;
+#define rep(i,a) for(int i=0; i<a;i++)
+#define rep1(i,a,b) for(int i=(a);i<=(b);++i)
+#define irep(i,b,a) for(int i=(b);i>=(a);--i)
+#define minv(v) *min_element(v.begin(),v.end())
+#define maxv(v) *max_element(v.begin(),v.end())
+#define each(it,s) for(auto it = s.begin(); it != s.end(); ++it)
+#define unq(v) sort(all(v)),(v).erase(unique((v).begin(),(v).end()),(v).end())
+#define _ios ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define sqr(a) ((a)*(a))
+#define sz(a) int(a.size())
+#define ff first
+#define ss second
+#define pb push_back
+#define inf (1<<29)
+
+const double pi = acos(-1.0);
+const int mod = 1000000007; // (int)1e9+7
+const int maxn = 2000100;
+
+vll prime;
+bitset<10000010>bs;
+
+void sieve(){
+        bs.set();
+        prime.pb(2LL);
+        for(ll i = 3; i<=10000000; i+=2){
+            if(bs[i]){
+                prime.pb(i);
+                for(ll j = i*i; j<=10000000; j+=i+i){
+                    bs[j] = 0;
+                }
+            }
+        }
+}
+
+int main(){
+        _ios;
+
+        sieve();
+        for(int i = 0; i<20; i++){
+            cout<<prime[i]<<endl;
+        }
+
+
+return 0;
+}
+
+
+
+
+
+
