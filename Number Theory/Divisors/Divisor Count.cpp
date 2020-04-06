020f904eb8a9e32bbfd72e88891637771f1a0132
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2020-04-06 10:03:46 +0600

    divisor count

---
+++ Number Theory/Divisors/Divisor Count.cpp
@@ -0,0 +1,59 @@
+
+// </> : towhid1zaman
+
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;typedef vector<ll> vll;
+#define fill(a) memset(a, 0, sizeof (a))
+#define all(v) (v).begin(),(v).end()
+#define sp(k) cout<<setprecision(k)<<fixed;
+#define rep(i,a) for(int i=0; i<a;i++)
+#define rep1(i,a,b) for(int i=(a);i<=(b);++i)
+#define irep(i,b,a) for(int i=(b);i>=(a);--i)
+#define minv(v) *min_element(v.begin(),v.end())
+#define maxv(v) *max_element(v.begin(),v.end())
+#define each(it,s) for(auto it = s.begin(); it != s.end(); ++it)
+#define unq(v) sort(all(v)),(v).erase(unique((v).begin(),(v).end()),(v).end())
+#define _ios ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define sqr(a) ((a)*(a))
+#define sz(a) int(a.size())
+#define ff first
+#define ss second
+#define pb push_back
+#define inf (1<<29)
+
+const double pi = acos(-1.0);
+const int mod = 1000000007; // (int)1e9+7
+const int maxn = 2000100;
+
+ll divcnt[maxn];
+
+void DivisorCount(ll n){
+    for(int i = 1; i<=n; i++){
+        for(int j  = i; j<=n; j+=i){
+            divcnt[j]++;
+        }
+    }
+}
+
+int main(){
+        _ios;
+
+        ll n; cin >> n;
+        DivisorCount(n);
+        for(int i = 1; i<=n; i++){
+            cout<<i<<" Has "<<divcnt[i]<<" divisors"<<endl;
+        }
+
+
+return 0;
+}
+
+
+
+
+
+
