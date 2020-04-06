5d60bed1f9f5cfd202c2a25bf970f73bc84957e1
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2020-04-06 12:39:09 +0600

    miller

---
+++ Number Theory/Primility Test/Miller Rabin Primality Test.cpp
@@ -0,0 +1,93 @@
+
+// </> : towhid1zaman
+
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;typedef vector<ll> vll;
+#define fill(a) memset(a, 0, sizeof (a))
+#define all(v) (v).begin(),(v).end()
+#define sp(k) cout<<setprecision(k)<<fixed;
+#define rep(i,a) for(int i=0; i<a;i++)
+#define rep1(i,a,b) for(int i=(a);i<=(b);++i)
+#define irep(i,b,a) for(int i=(b);i>=(a);--i)
+#define minv(v) *min_element(v.begin(),v.end())
+#define maxv(v) *max_element(v.begin(),v.end())
+#define each(it,s) for(auto it = s.begin(); it != s.end(); ++it)
+#define unq(v) sort(all(v)),(v).erase(unique((v).begin(),(v).end()),(v).end())
+#define _ios ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define sqr(a) ((a)*(a))
+#define sz(a) int(a.size())
+#define ff first
+#define ss second
+#define pb push_back
+#define inf (1<<29)
+
+const double pi = acos(-1.0);
+const int mod = 1000000007; // (int)1e9+7
+const int maxn = 2000100;
+
+//Calculate ( a*b ) %c
+ll mulmod(ll a,ll b,ll c)
+{
+    ll x = 0 , y = a%c ;
+    while(b>0)
+    {
+        if(b%2) x = (x+y)%c ;
+        y=(y*2)%c;
+        b/=2;
+    }
+    return x%c ;
+}
+
+//Calculate ( a^b ) %c
+ll modulo(ll a,ll b,ll c)
+{
+    ll x = 1 , y = a%c ;
+    while( b > 0 )
+    {
+        if(b%2) x = mulmod(x,y,c) ;
+        y=mulmod(y,y,c);
+        b/=2;
+    }
+    return x%c ;
+}
+
+bool miller(ll p, int  iteration=20)
+{
+    if(p<2) return false ;
+    if(p%2==0&&p!=2) return false ;
+
+    ll s = p-1 ;
+    while(s%2==0) s/=2;
+
+    for(ll i=1;i<=iteration;i++)
+    {
+        ll a = rand() % (p-1)+1 , temp = s ;
+
+        ll mod = modulo(a,temp,p);
+        while(mod!=1&&mod!=p-1&&temp!=p-1)
+        {
+            mod = mulmod(mod,mod,p);
+            temp*=2;
+        }
+        if(temp%2==0&&mod!=p-1) return false ;
+    }
+    return true ;
+}
+int main(){
+        _ios;
+
+        ll n; cin >> n;
+        if(miller(n))cout<<"Prime "<<endl;
+        else cout<<"Not Prime "<<endl;
+
+return 0;
+}
+
+
+
+
+
