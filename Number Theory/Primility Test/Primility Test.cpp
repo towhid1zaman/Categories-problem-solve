d043d66822fb4c3e96ea2c5f6faf93b4e5657b08
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2020-04-06 12:41:43 +0600

    primility testing

---
+++ Number Theory/Primility Test/Primility Test.cpp
@@ -0,0 +1,62 @@
+
+// </> : towhid1zaman
+
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;typedef vector<ll> vll;
+#define fill(a) memset(a, 0, sizeof (a))
+#define all(v) (v).begin(),(v).end()
+#define sp(k) cout<<setprecision(k)<<fixed;
+#define rep(i,a) for(int i=0; i<a;i++)
+#define rep1(i,a,b) for(int i=(a);i<=(b);++i)
+#define irep(i,b,a) for(int i=(b);i>=(a);--i)
+#define minv(v) *min_element(v.begin(),v.end())
+#define maxv(v) *max_element(v.begin(),v.end())
+#define each(it,s) for(auto it = s.begin(); it != s.end(); ++it)
+#define unq(v) sort(all(v)),(v).erase(unique((v).begin(),(v).end()),(v).end())
+#define _ios ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define sqr(a) ((a)*(a))
+#define sz(a) int(a.size())
+#define ff first
+#define ss second
+#define pb push_back
+#define inf (1<<29)
+
+const double pi = acos(-1.0);
+const int mod = 1000000007; // (int)1e9+7
+const int maxn = 2000100;
+
+bool isPrime(ll n){
+        if(n==2){
+            return true;
+        }
+        if(n<2 || n%2==0){
+            return false;
+        }
+        for(ll i = 3; i*i<=n;i+=2){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+}
+
+int main(){
+        _ios;
+
+        ll n; cin >> n;
+        if(isPrime(n))cout<<"Prime "<<endl;
+        else cout<<"Not Prime "<<endl;
+
+
+return 0;
+}
+
+
+
+
+
+
