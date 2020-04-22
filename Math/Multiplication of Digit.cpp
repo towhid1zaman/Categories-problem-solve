c186d6d1705bd1ce3c2409b50572ba5da60951b2
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2020-04-23 01:42:32 +0600

    multiOd

---
+++ Math/Multiplication of Digit.cpp
@@ -0,0 +1,50 @@
+
+// </> : towhid1zaman
+
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;typedef vector<ll> vll;
+#define fill(a,b) memset(a, b, sizeof (a))
+#define all(v) (v).begin(),(v).end()
+#define sp(k) cout<<setprecision(k)<<fixed;
+#define rep(i,a) for(int i=0; i<a;i++)
+#define rep1(i,a,b) for(int i=(a);i<=(b);++i)
+#define irep(i,b,a) for(int i=(b);i>=(a);--i)
+#define minv(v) *min_element(all(v))
+#define maxv(v) *max_element(all(v))
+#define unq(v) sort(all(v)),(v).erase(unique(all(v)),(v).end())
+#define _ios ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define sqr(a) ((a)*(a))
+#define sz(a) int(a.size())
+#define ff first
+#define ss second
+#define pb push_back
+
+const double pi = acos(-1.0);
+const int mod = 1000000007; // (int)1e9+7
+const int maxn = 2000100;
+int f(int n){// return multi of digit without 0
+    if(n == 0)return 0;
+    int res = 1;
+    while(n){
+        while(n%10==0){
+            n/=10;
+        }
+        res*=(n%10);
+        n/=10;
+    }
+    return res;
+}
+
+int main(){
+        _ios;
+
+        int n; cin >> n;
+        cout<<f(n)<<endl;
+
+
+return 0;
+}
+
