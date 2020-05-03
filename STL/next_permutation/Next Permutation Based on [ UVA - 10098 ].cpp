6322c1739d9f620283ee4ec336689f7036ef15ea
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2020-05-03 11:58:08 +0600

    next_permutation

---
+++ STL/next_permutation/Next Permutation Based on [ UVA - 10098 ].cpp
@@ -0,0 +1,68 @@
+
+// </> : towhid1zaman
+
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;typedef vector<ll> vll;
+#define fill(a,b) memset(a, b, sizeof (a))
+#define all(v) (v).begin(),(v).end()
+#define sp(k) cout<<setprecision(k)<<fixed;
+#define rep(i,a) for(int i=0; i<a;i++)
+#define rep1(i,a,b) for(int i=(a);i<=(b);++i)
+#define irep(i,b,a) for(int i=(b);i>=(a);--i)
+#define minv(v) *min_element(all(v))
+#define maxv(v) *max_element(all(v))
+#define unq(v) sort(all(v)),(v).erase(unique(all(v)),(v).end())
+#define _ios ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define sqr(a) ((a)*(a))
+#define sz(a) int(a.size())
+#define ff first
+#define ss second
+#define pb push_back
+
+const double pi = acos(-1.0);
+const int mod = 1000000007; // (int)1e9+7
+const int maxn = 2000100;
+
+int main(){
+        _ios;
+
+        int T; cin >> T;
+        while(T--){
+            string s; cin >> s;
+            sort(all(s));
+            do{
+                cout<<s<<endl;
+            }while(next_permutation(all(s)));
+
+            cout<<endl;
+        }
+
+
+return 0;
+}
+/*
+3
+ab
+abc
+bca
+
+
+ab
+ba
+abc
+acb
+bac
+bca
+cab
+cba
+abc
+acb
+bac
+bca
+cab
+cba
+
+*/
