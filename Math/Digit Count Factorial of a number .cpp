1848812e9f5640f93d18c3c1762e2ff592a9b4dc
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2020-05-17 23:23:34 +0600

    factDigitCount

---
+++ Math/Digit Count Factorial of a number .cpp
@@ -0,0 +1,58 @@
+
+// </> : towhid1zaman
+
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair< int, int > pii;
+typedef pair< pii, int > ppi;
+#define fill(a,b) memset(a,b,sizeof(a))
+#define all(v) (v).begin(),(v).end()
+#define sp(k) cout<<setprecision(k)<<fixed;
+#define rep(i,a) for(int i=0;i<a;i++)
+#define rep1(i,a,b) for(int i=(a);i<=(b);++i)
+#define irep(i,b,a) for(int i=(b);i>=(a);--i)
+#define minv(v) *min_element(all(v))
+#define maxv(v) *max_element(all(v))
+#define unq(v) (v).erase(unique(all(v)),(v).end())
+#define _ ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl "\n"
+#define sqr(a) ((a)*(a))
+#define sz(a) int(a.size())
+#define ff first
+#define ss second
+#define pb push_back
+
+const double pi = acos(-1.0);
+const int mod = 1000000007; // (int)1e9+7
+const int inf = 0x3f3f3f3f;
+const int eps = 1e-9;
+const int maxn = 2000100;
+
+void factDigit(){
+    ll n; cin >> n;
+    if(n<=1)cout<<1<<endl;
+    else{
+        double res = 0;
+        for(int i = 2; i<=n;i++){
+            res+=log10(i);
+        }
+        ll ans = floor(res)+1;
+        cout<<ans<<endl;
+    }
+}
+
+int main(){
+        _
+        int T; cin >> T;
+        while(T--){
+            factDigit();
+        }
+
+
+return 0;
+}
+
+
