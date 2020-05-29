e6682107566b98326c987298cb14f9927e2339b0
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2020-05-29 14:48:03 +0600

    sod, div

---
+++ Number Theory/Divisors/divisors and sod(easy).cpp
@@ -0,0 +1,21 @@
+
+// </> : towhid1zaman
+
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+
+int main(){
+
+  ll n; cin >> n;
+  ll res = 0;
+  for(ll i = 1; i<=n/2; i++){
+    if(n%i==0){
+      cout<<i<<" ";
+      res+=i;
+    }
+   }
+   cout<<n<<endl;
+   cout<<"SUM"<<res<<endl;
+
+}
