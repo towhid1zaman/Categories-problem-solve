f367b889c8f4cdee38833b32226eb93af6f0d6da
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2020-05-30 15:44:37 +0600

    Create binomial coefficient(n!Devide(k!*(n-k)!) [ncr].cpp

---
+++ Combinatorics/binomial coefficient(n!Devide(k!*(n-k)!) [ncr].cpp
@@ -0,0 +1,31 @@
+
+// </> : towhid1zaman
+
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+
+
+ll ncr(ll n, ll r){
+    ll res = 1;
+    ll range = min(r, n-r);
+    for(ll i = 1; i<=range; i++){
+        res *= (ceil(n-i+1)/i);
+    }
+    return res;
+}
+
+int main(){
+        ll n,m;
+        while(cin >> n >> m){
+            if(n==0 and m==0)break;
+            ll res = ncr(n,m);
+            cout<<res<<endl;
+        }
+
+
+
+return 0;
+}
+
+
