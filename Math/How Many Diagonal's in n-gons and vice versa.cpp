cdf7e015bbccfbacfaeefbfa1fe378dbee1e5e25
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2020-06-02 14:25:16 +0600

    diagonals, polygons

---
+++ Math/How Many Diagonal's in n-gons and vice versa.cpp
@@ -0,0 +1,32 @@
+
+// </> : towhid1zaman
+
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+
+int main(){
+        ll n;
+        while(cin >> n){
+            if(n==0)break;
+            ll diagonal = ( n*(n-3) )/ 2;
+            ll gons = ceil( (3 + sqrt(9+(8*n)))/2   );
+            cout << diagonal <<' '<< gons << endl;
+        }
+
+
+return 0;
+}
+
+/*
+how many diagonal in n-gons = n*(n-3)/2 ;
+
+if we cant do its opposite we can find gon's 
+    
+    its just ax^2+bx+c=0 formula.
+    see, n*(n-3)/2 = c(its given)
+            => n^-3n - 2c = 0
+
+*/
+
+
