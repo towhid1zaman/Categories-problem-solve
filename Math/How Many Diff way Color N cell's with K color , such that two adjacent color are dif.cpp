c5027677b8a873a2234f3d75f24a30863b109681
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2020-06-08 16:19:32 +0600

    adjacent color.

---
+++ Math/How Many Diff way Color N cell's with K color , such that two adjacent color are dif.cpp
@@ -0,0 +1,22 @@
+/*
+How Many Diff way Color N cell's with K color , 
+such that two adjacent color are dif.
+*/
+
+
+// </> : towhid1zaman
+
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+
+int main(){
+        _
+        int n,k; cin >> n >> k;
+        ll res = k* pow((k-1),n-1);
+        cout<<res<<endl;
+
+return 0;
+}
+
+
