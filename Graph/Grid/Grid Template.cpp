ba1d2c6f1be06334e2642e5c9e5c4881c13e0293
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2020-06-16 14:45:15 +0600

    grid

---
+++ Graph/Grid/Grid Template.cpp
@@ -0,0 +1,9 @@
+const int kx[8]={-2,-2,-1,1,-1,1,2,2}; //knight
+const int ky[8]={1,-1,-2,-2,2,2,-1,1};
+const int dx8[8] = {0,0,1,1,1,-1,-1,-1};
+const int dy8[8] = {1,-1,1,-1,0,0,-1,1};
+const int dx4[4] = {0,01,-1}, dy4[4] = {1,-1,0,0};
+
+bool gridCheck(int x, int y,int n, int m) { 
+	return x >= 0 && y >= 0 && x < n && y < m; 
+}
