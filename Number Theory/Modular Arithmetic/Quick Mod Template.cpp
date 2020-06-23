79a9e37ddb005886e87e2373201170a3b76c637f
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2020-06-23 19:40:18 +0600

    Update Quick Mod Template.cpp

--- Number Theory/Modular Arithmetic/Quick Mod Template.cpp
+++ Number Theory/Modular Arithmetic/Quick Mod Template.cpp
@@ -7,12 +7,11 @@ inline ll MOD(ll x)
 // if we want to calculate mod inverse, then simply we pass m-2
 ll modexpo(ll x, ll n, ll m)
 {
-    ll ret = 1;
-    while(n)
-    {
-        if(n & 1LL) ret = (ret*x) % m;
+    ll res = 1;
+    while(n){
+        if(n & 1LL) res = (res*x) % m;
         x = (x*x) % m;
         n >>= 1;
     }
-    return ret;
+    return res;
 }
