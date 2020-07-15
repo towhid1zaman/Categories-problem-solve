cee18ec1909d9559db67907bb7646e94219dfb64
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2020-07-15 22:56:21 +0600

    grid

--- Graph/Grid/Grid Template.cpp
+++ Graph/Grid/Grid Template.cpp
@@ -1,7 +1,12 @@
-const int kx[8] = {-2, -2, -1, 1, -1, 1, 2, 2}, ky[8] = {1, -1, -2, -2, 2, 2, -1, 1}; //knight
-const int dx8[8] = {1, 0, -1, 0, 1, 1, -1, -1}, dy8[8] = {0, 1, 0, -1, -1, 1, -1, 1}; //king
+//const int kx[8] = {-2, -2, -1, 1, -1, 1, 2, 2}, ky[8] = {1, -1, -2, -2, 2, 2, -1, 1}; //knight
+//const int dx8[8] = {1, 0, -1, 0, 1, 1, -1, -1}, dy8[8] = {0, 1, 0, -1, -1, 1, -1, 1}; //king
 const int dx4[4] = {1,0,-1,0}, dy4[4] = {0,1,0,-1};
-
+// UP, RIGHT, DOWN, LEFT, UPPER-RIGHT, LOWER-RIGHT, LOWER-LEFT, UPPER-LEFT
+const int dx8[8] = {-1, 0, 1, 0, -1, 1,  1, -1};
+const int dy8[8] = { 0, 1, 0,-1,  1, 1, -1, -1};
+// Represents all moves of a knight in a chessboard
+const int dxKnightMove[8] = {-1, -2, -2, -1,  1,  2, 2, 1};
+const int dyKnightMove[8] = { 2,  1, -1, -2, -2, -1, 1, 2};
 bool gridCheck(int x, int y,int n, int m) { 
     return x >= 0 && y >= 0 && x < n && y < m; 
 }
