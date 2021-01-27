f1b7933c1da847abc6dc103254ad740b7218f4f2
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2021-01-28 03:27:01 +0600

    n^2 + m^2 = k^2

---
+++ Math/Find The Pythagorean Triples.cc
@@ -0,0 +1,74 @@
+#pragma comment (linker,"/STACK:16777216")
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+#define all(v) (v).begin(),(v).end()
+#define endl "\n"
+const int maxn = 200005;
+const int mod = 1000000007;
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+/*
+assume, n < k < m
+
+    n^2 + m^2 = k^2
+->  n^2 = k^2 - m^2
+->  (k+m)(k-m) = n^2 * 1
+so, k = (n^2 + 1)/2 , m = (n^2 - 1)/2;
+*/
+void task(){
+    ll n; cin >> n;
+    if(n<3){
+        cout << -1 << endl;
+        return;
+    }
+    ll n2 = n*n;
+    ll k, m;
+    if(n%2){
+        cout << (n2+1)/2 << ' '<<(n2-1)/2<<endl;
+    }
+    else{
+        cout << (n2+4)/4 <<' '<<(n2-4)/4<<endl;
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+#ifdef OJ
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int T = 1; //cin >> T;
+    for(int __ = 1; __ <= T; __++){
+        //cout <<"Case "<<__<<": ";
+        task();
+    }
+      
+return 0;
+}
