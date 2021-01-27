baa5403095d092627f7468dd616d04ffe18d7a8e
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2021-01-27 13:17:42 +0600

    n<10

---
+++ Backtracking-Recursion/permutation recursion (n<10).cpp
@@ -0,0 +1,54 @@
+#pragma comment (linker,"/STACK:16777216")
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+#define all(v) (v).begin(),(v).end()
+#define endl "\n"
+const int maxn = 200005;
+const int mod = 1000000007;
+
+int total;
+void recur(int n, std::vector<int>per){
+    if(per.size() == n){
+        for(auto x: per)cout << x <<' ';cout << endl;
+        total++;
+    }
+    else{
+        std::map<int, bool>exist;
+        for(auto x: per){
+            exist[x] = true;
+        }
+        for(int i =1; i<=n; i++){
+            if(!exist[i]){ // check weather current value present or not
+                std::vector<int>newper = per;
+                newper.push_back(i);
+                recur(n, newper); // calling another one
+            }
+        }
+    }
+}
+void task(){
+    // total possible permutation we know, N!
+    // 3! = 1*2*3 = 6;
+    // worked fine if n<10
+    int n; cin >> n;
+    recur(n, std::vector<int>());
+    cout << total << endl;
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+#ifdef OJ
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int T = 1; //cin >> T;
+    for(int __ = 1; __ <= T; __++){
+        //cout <<"Case "<<__<<": ";
+        task();
+    }
+      
+return 0;
+}
