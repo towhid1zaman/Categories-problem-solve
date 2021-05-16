0bdbaf005ba72f926d657f4c852aad85bf739e25
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2021-05-16 09:50:50 +0600

    divisor count, sieve

--- Number Theory/Divisors/Divisor Count Using Prime Factorization.cpp
+++ Number Theory/Divisors/Divisor Count Using Prime Factorization.cpp
@@ -1,20 +1,20 @@
 //quick sieve
 bool checkprime[maxn + 5];
-vll vc;
+vll prime;
 void sieve() {///false means prime
-    vc.pb(2);
+    prime.pb(2);
     checkprime[0]=true;
     checkprime[1]=true;
     for (ll i = 3; i <= maxn; i += 2) {
         if (!checkprime[i]) {
-            vc.pb(i);
+            prime.pb(i);
             for (ll j = i * i; j <= maxn; j += (i + i))
                 checkprime[j] = true;
         }
     }
 }
 //DivisorCount using primefactorization
-void divisor(ll n){
+ll divisor(ll n){
     ll ans = 1;
     for(ll i = 0; i<prime.size() and prime[i]*prime[i]<=n; i++){
         if(n%prime[i]==0){
