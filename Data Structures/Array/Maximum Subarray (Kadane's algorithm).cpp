15594f6b3a09cb2260705484f55a2a0f2a2eabf6
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2021-07-06 21:51:33 +0600

    Kadane's algorithm

---
+++ Data Structures/Array/Maximum Subarray (Kadane's algorithm).cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int ans = INT_MIN;
+        int a = 0;
+        for(int x: nums){
+            a+=x;
+            ans = max(ans, a);
+            a = max(0, a);
+        }
+        return ans;
+    }
+};
