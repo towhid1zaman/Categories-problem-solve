e7a5385b42d07b2e1736ce7d5b1bb79716174c66
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2021-07-07 13:03:04 +0600

    Recursion

---
+++ Backtracking-Recursion/BasicProblems/Subsets Print.cpp
@@ -0,0 +1,30 @@
+
+/*
+    Given an integer array nums of unique elements, return  all possible subsets (the power set).
+    total subsets = 2^N
+
+*/
+
+class Solution {
+public:
+    vector<vector<int>>suball;
+    void printSubsets(vector<int>nums, int pos, int n, vector<int> ans){
+        if(pos >= n){
+            suball.push_back(ans);
+            return;
+        }
+        //take
+        ans.push_back(nums[pos]);
+        printSubsets(nums, pos+1, n, ans);
+        
+        //dont take
+        ans.pop_back();
+        printSubsets(nums, pos+1, n, ans);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n = (int)nums.size();
+        vector<int>a;
+        printSubsets(nums, 0, n, a);
+        return suball;
+    }
+};
